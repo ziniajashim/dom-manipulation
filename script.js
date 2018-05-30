936dd8c7c16075439ad8e571fa5cdbd8a9ef259a
Zinia Binte Jashim <1431069@iub.edu.bd>
2018-05-30 04:19:52 +0000

    classwork 4 2a

--- script.js
+++ script.js
@@ -3,6 +3,14 @@ var heading = document.getElementById('heading')
 console.log(heading.innerText)
 
 /* Task 2 using plain JavaScript */
+//Task 2a
+var textBtn = document.getElementById('textBtn2')
+textBtn.addEvenListener('click',function(){
+    var task2 = document.getElementById('task2a')
+    var paragraph = document.createElement('p')
+    paragraph.innerText = "Hello World"
+    task2.appendChild(paragraph)
+})
 
 
 
